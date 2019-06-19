f704ae7227b1a9c3b4e02b5f8eb4cdd84fc18217
unknown <tgchen@wynetech.com>
2019-06-19 11:54:46 -0700

    clean form and verify email

--- basicapp/forms.py
+++ basicapp/forms.py
@@ -1,10 +1,19 @@
 from django import forms
 from django.core import validators
 
+
+
 class FormName(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
+    verify_email = forms.EmailField(label='Enter your Email again:')
     text = forms.CharField(widget=forms.Textarea)
-    botcatcher = forms.CharField(required=False,
-                                 widget=forms.HiddenInput,
-                                 validators=[validators.MaxLengthValidator(0)])
+
+    def clean(self):
+        all_clean_data = super().clean()
+        email = all_clean_data['email']
+        verify_email = all_clean_data['verify_email']
+
+        if email != verify_email:
+            raise forms.ValidationError("Make sure Email Match")
+            
